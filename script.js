12f1f7fdf8eb30de1532db9f9e74f3ff21a23dd5
jazminLU <jluna2554@email.com>
2023-10-09 19:54:35 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+
+    // Función para agregar una nueva tarea
+    function agregarTarea() {
+        const nuevaTareaInput = document.getElementById("nuevaTarea");
+        const listaTareas = document.getElementById("listaTareas");
+        const nuevaTareaTexto = nuevaTareaInput.value.trim();
+        if (nuevaTareaTexto !== "") {
+        const nuevaTarea = document.createElement("li");
+        nuevaTarea.textContent = nuevaTareaTexto;
+        const botonEliminar = document.createElement("button");
+        botonEliminar.textContent = "Eliminar";
+        botonEliminar.className = "delete";
+        botonEliminar.onclick = function() {
+        listaTareas.removeChild(nuevaTarea);
+        };
+        nuevaTarea.appendChild(botonEliminar);
+        listaTareas.appendChild(nuevaTarea);
+        nuevaTareaInput.value = "";
+        }
+        }
+        document.addEventListener("DOMContentLoaded", function () {
+            // Función para mostrar tareas completadas
+            function mostrarCompletadas() {
+              const tareas = document.querySelectorAll("li");
+              tareas.forEach(tarea => {
+                if (tarea.classList.contains("completed")) {
+                  tarea.style.display = "flex";
+                } else {
+                  tarea.style.display = "none";
+                }
+              });
+            }
+          
+            // Función para mostrar tareas pendientes
+            function mostrarPendientes() {
+              const tareas = document.querySelectorAll("li");
+              tareas.forEach(tarea => {
+                if (!tarea.classList.contains("completed")) {
+                  tarea.style.display = "flex";
+                } else {
+                  tarea.style.display = "none";
+                }
+              });
+            }
+          
+            // Agregar evento de clic al botón "Mostrar Completadas"
+            document.getElementById("mostrarCompletadas").addEventListener("click", mostrarCompletadas);
+          
+            // Agregar evento de clic al botón "Mostrar Pendientes"
+            document.getElementById("mostrarPendientes").addEventListener("click", mostrarPendientes);
+          });
+          
